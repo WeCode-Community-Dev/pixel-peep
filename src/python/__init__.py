e9083c9108a09b64c3c45ebd516f7c24545b839b
Arjun E <arjun.e@astriadigital.com>
2025-04-22 21:41:19 +0530

    improved the compare image function perfomace

--- src/python/__init__.py
+++ src/python/__init__.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from skimage.metrics import structural_similarity as ssim
 from .normalize_image import normalize_for_comparison
 from .pHash import compare_phash
@@ -12,7 +13,16 @@ def compare_images(img1_path, img2_path):
     """Core comparison workflow with debugging"""
     img1 = normalizing_image(img1_path)
     img2 = normalizing_image(img2_path)
+
+    if img1.shape != img2.shape:
+        return False, 0.0
     
+    # 3. Color histogram comparison (fast)
+    hist1 = cv2.calcHist([img1], [0,1,2], None, [8,8,8], [0,256,0,256,0,256])
+    hist2 = cv2.calcHist([img2], [0,1,2], None, [8,8,8], [0,256,0,256,0,256])
+    hist_score = cv2.compareHist(hist1, hist2, cv2.HISTCMP_CORREL)
+    if hist_score > 0.9:
+        return True, hist_score
     
     # Fast pHash check
     phash_diff = compare_phash(img1, img2)
