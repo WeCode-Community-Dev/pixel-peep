78401c01c527aa945badac8ff3f6c65af14200d8
the-specs-coder <nhsanjay24@gmail.com>
2025-05-04 18:06:10 +0530

    Changed the variable names of folder directories variables

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 import test,train
 
-folder_dir1="C:/Users/Sanjay/OneDrive/Documents/pixel-peep/test_cases/train_images"
-folder_dir2="C:/Users/Sanjay/OneDrive/Documents/pixel-peep/test_cases/test_images"
+train_folder_dir="C:/Users/Sanjay/OneDrive/Documents/pixel-peep/test_cases/train_images"
+test_folder_dir="C:/Users/Sanjay/OneDrive/Documents/pixel-peep/test_cases/test_images"
 
-des_list=train.train_images(folder_dir=folder_dir1)
-is_Match=test.test_images(folder_dir=folder_dir2,des_from_train=des_list)
+des_list=train.train_images(folder_dir=train_folder_dir)
+is_Match=test.test_images(folder_dir=test_folder_dir,des_from_train=des_list)
 
 for matches in is_Match:
     print(matches,sep="\n")
